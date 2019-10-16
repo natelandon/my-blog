be53ad4f1baff723bf1ee968fb2dc2604fc1022f
natelandon <njlandon@gmail.com>
2019-10-16 10:22:37 -0500

    First Commit

---
+++ src/build/precache-manifest.13916bf455208fa574de3f145ec22e00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c583c34ab5e5d37c1012185f98a46ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f2a9735e5a9b064320c",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "3216412602ee3b41ef4f",
+    "url": "/static/js/2.143332d2.chunk.js"
+  },
+  {
+    "revision": "0f2a9735e5a9b064320c",
+    "url": "/static/js/main.060972a7.chunk.js"
+  },
+  {
+    "revision": "19dffa7a1e78686fab4a",
+    "url": "/static/js/runtime-main.ca086102.js"
+  }
+]);
